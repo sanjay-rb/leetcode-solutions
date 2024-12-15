31a59afcd34fb9433574ecdd52f9e97850fa429d
sanjay-rb <sanjaybabu618@gmail.com>
2024-12-15 16:07:07 +0530

    Completed Contains Duplicate

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,3 +1,10 @@
+from typing import List
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        pass
+        return len(nums) != len(set(nums))
+
+s = Solution()
+print(s.containsDuplicate([1,2,3,1]))
+print(s.containsDuplicate([1,2,3,4]))
+print(s.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
